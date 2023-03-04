70131465db80c555e154688260c8695163a9d41f
Emmanuel Gacel <evg2110@columbia.edu>
2023-03-04 11:03:20 -0500

    Added error checking functions

--- src/sort.c
+++ src/sort.c
@@ -20,6 +20,8 @@ int main(int argc, char **argv) {
 	int iflag = 0; 
 	int dflag = 0; //Sets all flags to false
 	int c;
+	int file_flag = 0;
+	int bytes = 0;
 	opterr = 0; //Supresses errors
 	while ((c = getopt(argc, argv, ":id")) != -1){//Checks the flags inputed by the user
 		printf("c: %c\n", c);
@@ -36,6 +38,56 @@ int main(int argc, char **argv) {
 		}
 	}
 	printf("iflag: %d\ndflag:  %d\n", iflag, dflag);
+	  int total_flags = iflag + dflag;
+        if (total_flags == 2){//Checks to see if too many flags were specified
+                fprintf(stderr, "Error: Too many flags specified.");
+                return EXIT_FAILURE;
+        }else if(total_flags == 0 && argc > 2){//Checks to see if too many files were specified
+                fprintf(stderr, "Error: Too many files specified.");
+                return EXIT_FAILURE;
+        }else if(total_flags == 1 && argc > 3){//Checks to see if too many files were specified
+                fprintf(stderr, "Error: Too many files specified.");
+                return EXIT_FAILURE;
+        }else if(total_flags == 0 && argc == 2){//Checks to see if a file was specifies
+                file_flag = 0;
+        }else if(total_flags == 1 && argc == 3){//Checks to see if a file was specified
+                file_flag = 0;
+        }else if(dflag == 1){//If
+                bytes = sizeof(double);
+        }
+        else{
+                bytes = sizeof(int);
+        }
+        int src_fd;
+        if(file_flag == 0 && dflag == 0){//Reads ints from stdin
+                int i = 0;
+                int num;
+                int buf[1024];
+                while(scanf("%d", &num) != EOF){
+                        buf[i] = num;
+                        i++;
+                }
+
+        }
+        else if(file_flag == 0){//Reads doubles from stdin
+                int i = 0;
+                double num;
+                double buf[1024];
+                while(scanf("%lf`", &num) != EOF){
+                        buf[i] = num;
+                        i = i + bytes;
+                }
+
+        }else{
+                if((src_fd = open(argv[argc], O_RDONLY)) == -1){
+                        fprintf(stderr, "Error: Cannot open source file '%s'. %s.\n", argv[argc], strerror(errno));
+                        return EXIT_FAILURE;
+
+                }else{
+
+                }
+        }
+
 	
 	//SCANS INTEGERS FROM STDIN
 	if(argc < 3){
